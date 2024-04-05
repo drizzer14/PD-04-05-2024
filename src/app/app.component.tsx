c33b330177a66bc5a66532cb0e7ebabb46db1244
drizzer14 <drizzer14@gmail.com>
2024-04-05 15:22:16 +0300

    initial commit

---
+++ src/app/app.component.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+import * as Styled from "./app.styles";
+
+const App: FC = () => {
+  return (
+    <Styled.App>
+      <Styled.Reset />
+    </Styled.App>
+  );
+};
+
+export default App;
---
+++ src/app/app.styles.ts
@@ -0,0 +1,69 @@
+import { createGlobalStyle, styled } from "styled-components";
+
+export const Reset = createGlobalStyle`
+/*
+    Josh's Custom CSS Reset
+    https://www.joshwcomeau.com/css/custom-css-reset/
+  */
+
+    *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+  }
+
+  html {
+    height: 100%;
+    font-size: 18px;
+    font-family: Oxanium;
+  }
+
+  body {
+    height: 100%;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  span, p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+    text-shadow: 0 0 1em currentColor;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  #root, #__next {
+    isolation: isolate;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }`;
+
+export const App = styled.div`
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`;
